afb4699274624d101a0dce4fe6bea05d631acc5c
kanatamon <kantpaon.sritiyot@gmail.com>
2020-07-08 16:00:07 +0700

    feat(feed): simple feed query

--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
 const { GraphQLServer } = require('graphql-yoga')
 
 const typeDefs = `
-type Query {
-  info: String!
-}
+  type Query {
+    info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
+  }
 `
+
+const links = [
+  {
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL',
+  },
+]
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  /*
+  The argument `parent` below is the result of the previous resolver execution
+  , which in this case `feed` in `Query` above is the previous resolver,
+  the result of the previous resovler is `links`.
+  On the other hand `parent` is a particular item of `links`
+  */
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
   },
 }
 
